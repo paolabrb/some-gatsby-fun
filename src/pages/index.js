a42d88cb4da1e3e08db255a43836fcbf363a7da0
Paola Barboglio <paola.barboglio@gmail.com>
2020-04-17 18:36:33 +0200

    turn btn into link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 import Navbar from '../components/navbar'
+import { Link } from 'gatsby'
+
 
 const indexPage = () => {
     return(
@@ -11,7 +13,7 @@ const indexPage = () => {
                     <div className="card">
                         <div className="card-content center">
                         <span className="card-title">Bored today?</span>
-                        <button className="waves-effect waves-light btn-small indigo darken-4">Random German Word!</button>
+                        <Link to="/random" className="waves-effect waves-light btn-small indigo darken-4">Random German Word!</Link>
                      </div>
                     </div>
                     </div>
