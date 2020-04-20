49ff122dddf15d184462bed7205347a08922dcc8
Paola Barboglio <paola.barboglio@gmail.com>
2020-04-20 12:24:36 +0200

    change functional component into class component to save randomWord to state"

--- src/pages/random.js
+++ src/pages/random.js
@@ -2,29 +2,43 @@ import React from 'react'
 import Navbar from '../components/navbar'
 import JSONData from '../../public/words.json'
 
-const RandomWord = () => {
-    let index = Math.floor(Math.random() * 345);
-    const randomWord = JSONData[index].word;
+class RandomWord extends React.Component {
+    constructor(props) {
+        super(props);
+        // Non chiamre this.setState() qui!
+        this.state = { randomWord: null };
+    };
 
-    return(
-        <div>
-        <Navbar />
-        <div className="card-container my-5">
-            <div className="row">
-                <div className="col s12 m6">
-                <div className="card">
-                    <div className="card-content center">
-                    <span className="card-title">Here you go!</span>
-                    <p>Your German Word is: { randomWord }</p>
-                    <button className="waves-effect waves-light btn-small indigo darken-4">Another one?</button>
-                 </div>
-                </div>
+
+    render() {
+        this.setState(state => {
+            let index = Math.floor(Math.random() * 345);
+            state.randomWord = JSONData[index].word;
+            console.log(this.state)
+        });
+
+        return(
+            <div>
+            <Navbar />
+            <div className="card-container my-5">
+                <div className="row">
+                    <div className="col s12 m6">
+                    <div className="card">
+                        <div className="card-content center">
+                        <span className="card-title">Here you go!</span>
+                        <p>Your German Word is: { this.state.randomWord }</p>
+                        <button className="waves-effect waves-light btn-small indigo darken-4">Another one?</button>
+                     </div>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    )
+        )
+    }    
 }
+
+
 
 
 export default RandomWord;
